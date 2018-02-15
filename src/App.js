38cde94a6f2f6a33a48502980e62cfe5bee42b37
Darien Duong <darienduong@gmail.com>
2018-02-15 09:34:29 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+// import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+// import Router from './Router';
+
+class App extends Component {
+  componentWillMount() {
+    var config = {
+      apiKey: 'AIzaSyBGG3LRuAuaEVoE70igVNw1M4djbgihD_0',
+      authDomain: 'first-auth-82fc0.firebaseapp.com',
+      databaseURL: 'https://first-auth-82fc0.firebaseio.com',
+      projectId: 'first-auth-82fc0',
+      storageBucket: 'first-auth-82fc0.appspot.com',
+      messagingSenderId: '59464127918'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
+
+export default App;
